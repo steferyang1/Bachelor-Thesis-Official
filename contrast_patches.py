aed4673709dbf8f14d939e5b3632b53e0d8b39f7
Steven <stevenyang83@hotmail.com>
2025-06-23 23:00:55 +0200

    added documentation to contrast_patches.py

--- contrast_patches.py
+++ contrast_patches.py
@@ -3,25 +3,21 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import os
 
-
-
-# Pad naar jouw afbeelding
 base_path = "C:/Users/steve/IdeaProjects/programming/BCI/Thesis/steven/Thesis/images/"
-filename = 'grating.png'  # ← vervang dit met de echte naam!
+filename = 'grating.png' 
 image_path = os.path.join(base_path, filename)
 
 
 
-# Laad de afbeelding
+# load image
 img = Image.open(image_path).convert('RGB')
 img_array = np.array(img)
 
-# Reshape naar 1D per kanaal
 r = img_array[:, :, 0].flatten()
 g = img_array[:, :, 1].flatten()
 b = img_array[:, :, 2].flatten()
 
-# Plot histogrammen
+# plot histogram
 plt.figure(figsize=(10, 4))
 plt.subplot(1, 3, 1)
 plt.hist(r, bins=256, color='red', alpha=0.7)
@@ -35,10 +31,10 @@ plt.title('B-kanaal')
 plt.tight_layout()
 plt.show()
 
-# Bereken contrast range per kanaal
+# here we calculate the contrast in the image
 for channel, name in zip([r, g, b], ['R', 'G', 'B']):
     min_val = np.min(channel)
     max_val = np.max(channel)
     contrast_range = max_val - min_val
     contrast_percentage = (contrast_range / 255) * 100
-    print(f'{name}-kanaal: min={min_val}, max={max_val}, contrast={contrast_percentage:.1f}%')
+    print(f'contrast={contrast_percentage:.1f}%')
